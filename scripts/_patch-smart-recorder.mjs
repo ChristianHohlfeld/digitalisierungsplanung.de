76453c4284dd2922986086ce9bb04610f39c6931
Christian Heinrich Hohlfeld <chris.hohlfeld@gmail.com>
2026-08-21 23:31:05 +0200

    Remove one-shot smart recorder patcher

--- scripts/_patch-smart-recorder.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'node:fs';
-const path='state.html';
-const state=fs.readFileSync(path,'utf8');
-const fixed=state.replaceAll('\\`','`').replaceAll('\\${','${');
-if(fixed!==state)fs.writeFileSync(path,fixed);
-console.log(fixed===state?'smart recorder source already clean':'smart recorder interpolations fixed');
